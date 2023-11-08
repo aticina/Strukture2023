dabf664b6f409d3521ead12cfa1dc5d3ac5c6c02
aticina <115451545+aticina@users.noreply.github.com>
2023-11-08 18:09:17 +0100

    Create Zad5.c

---
+++ Zad5.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_CHAR 1000
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define MALLOC_FAIL -1
+
+int main(){
+  return EXIT_SUCCESS;
+}
+/*
+neki int read file
+stog je jednostruko vezana lista s pravilom LIFO last in first out
+ima funkciju push(puni stog) i pop(prazni stog)
+pravimo se da su nam operatori jednako bitni? 
+22*3-5+33**
+naš stog prima samo brojeve tj elementi liste imaju samo double i pokazivač
+while/strlen(currentbuffer))>0){
+status=sscanf(currentbuff, %lf %n",&number, &numbytes)
+if(status!=1){
+  sscanf(currentbuffer," %c %n", &operation, &numbytes)
+  status=popandpreformoperation(head,operation,result)
+  if(status !=EXIT_SUC)
+    return EXIT_F
+
+  number=*result
+}
+newstackelement=createstackelelmentt(number)
+if(!newstackelement) return exit_value
+}
+current buffer+= numbytes
+printf("I%sI <-->" currentbuffer):;
+push(head, newstackelement)
+}ovdje valjda zavrsava while
+return checkstackandextractresult(head, result)
+
+int
+
+int popandpreformoperation(position head, char operation, double* result){
+  double operand1=0
+  double operand2=0
+  int status1=0
+  int status2=0
+
+status1=pop(head,&operand1)
+  if(status1!=EXIT_SUCCESS)
+      return EXIT_FAIL
+
+status2=pop(head,&operand2)
+  if(status2!=EXIT_SUC
+    return EXIT_F
+
+switch(operation)
+case "+" *result=operand1+operand2 isto za - * / i default
+
+
+push prima head i novi elemenent je pratkički dodaj na početak liste
+{new->head->next}
+pop
+*/
